bc89412152a66e15bfe2b475471ba0b32c214da0
Srikanth Kaza <kazaonknowledge@gmail.com>
2015-12-26 09:42:47 +0530

    Added arrays chapter

---
+++ Arrays/P8.1.c
@@ -0,0 +1,18 @@
+/*P8.1 Program to input values into an array and display them*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	int arr[5], i;
+	for (i=0; i<5; i++) {
+		printf("Enter the value for arr[%d]:", i);
+		scanf("%d",&arr[i]);
+	}
+
+	printf ("The array elements are:\n");
+	for (i=0; i<5; i++)
+		printf ("%d\t",arr[i]);
+	printf ("\n");
+	return 0;
+}
